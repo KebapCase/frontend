2568662f17cbe4ba45fb3e39b17f854e74decf92
Evgenuy Petrushevski <evgenuypetrushevski@gmail.com>
2018-02-20 00:48:19 +0300

    MR-1 Update SignUp component.
    Added form for user credentials.

--- src/app/authorization/sign-up/sign-up.component.ts
+++ src/app/authorization/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {RegistrationModel} from "../../models/RegistrationModel";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  registrationForm: FormGroup;
+  model: RegistrationModel = new RegistrationModel();
+
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.registrationForm = this.fb.group({
+      'username': [this.model.userName, Validators.required],
+      'email': [this.model.email, [Validators.email, Validators.required]],
+      'password': [this.model.password, Validators.required]
+    });
   }
 
 }
