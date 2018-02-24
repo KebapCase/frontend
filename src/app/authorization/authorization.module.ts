c02c352ff138679aa261e4ee613150816e2525d6
Evgenuy Petrushevski <evgenuypetrushevski@gmail.com>
2018-02-24 16:01:32 +0300

    MR-1 Moved the modal window to the authorize-modal component.

--- src/app/authorization/authorization.module.ts
+++ src/app/authorization/authorization.module.ts
@@ -4,6 +4,7 @@ import { SingInComponent } from './sing-in/sing-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ReactiveFormsModule} from '@angular/forms';
 import { SocialComponent } from './social/social.component';
+import { AuthorizeModalComponent } from './authorize-modal/authorize-modal.component';
 
 
 @NgModule({
@@ -11,7 +12,7 @@ import { SocialComponent } from './social/social.component';
     CommonModule,
     ReactiveFormsModule
   ],
-  declarations: [SingInComponent, SignUpComponent, SocialComponent],
-  exports: [SingInComponent, SignUpComponent, SocialComponent]
+  declarations: [SingInComponent, SignUpComponent, SocialComponent, AuthorizeModalComponent],
+  exports: [SingInComponent, SignUpComponent, SocialComponent, AuthorizeModalComponent]
 })
 export class AuthorizationModule { }
---
+++ src/app/authorization/authorize-modal/authorize-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorize-modal',
+  templateUrl: './authorize-modal.component.html',
+  styleUrls: ['./authorize-modal.component.css']
+})
+export class AuthorizeModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
