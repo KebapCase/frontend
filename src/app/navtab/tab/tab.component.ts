18311bb9aa07ebb8ca8fea7bb087508cce7923d7
Evgenuy Petrushevski <evgenuypetrushevski@gmail.com>
2018-03-19 23:21:59 +0300

    MR-12 Add fade in animation for tab toggling.

--- src/app/navtab/tab/tab.component.ts
+++ src/app/navtab/tab/tab.component.ts
@@ -1,10 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TabsComponent } from '../tabs/tabs.component';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      transition('void => *', [
+        style({opacity: 0}),
+        animate(400)
+      ])
+    ])
+  ]
 })
 export class TabComponent {
 
