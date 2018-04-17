2fe42a82339a45bd304f5e6d3fac8b118a3b0499
Andru Shakarov <Anron2012@mail.ru>
2018-04-17 12:09:18 +0300

    drag-and-drop.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { ModalComponent } from './modal/modal.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {PreviewComponent} from './components/preview/preview.component';
+import {DragAndDropComponent} from './components/drag-and-drop/drag-and-drop.component';
+import {Ng2FileDropModule} from 'ng2-file-drop';
+import {Ng2CloudinaryModule} from 'ng2-cloudinary';
+import {FileUploadModule} from 'ng2-file-upload';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalComponent,
-    PreviewComponent
+    PreviewComponent,
+    DragAndDropComponent
   ],
   imports: [
     BrowserModule,
     AuthorizationModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    Ng2FileDropModule,
+    Ng2CloudinaryModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
