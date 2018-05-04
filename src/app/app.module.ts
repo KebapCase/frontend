a80b886173eb114db4a4a5c3419e06f389f49eda
Evgenuy Petrushevski <evgenuypetrushevski@gmail.com>
2018-05-04 21:14:07 +0300

    MR-11 Fix after merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,24 @@ import { ModalComponent } from './modal/modal.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavbarComponent } from './navbar/navbar.component';
 import {PreviewComponent} from './components/preview/preview.component';
+import {AuthService} from './services/auth.service';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ModalComponent,
-    PreviewComponent
-    ModalComponent,
+    PreviewComponent,
     NavbarComponent
   ],
   imports: [
     BrowserModule,
     AuthorizationModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
